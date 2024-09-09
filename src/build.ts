b8bc0d5efb4eb326eb61c4f19df180ee814adce9
Felix Ranesberger <felix.ranesberger@in2code.de>
2024-09-09 10:32:25 +0200

    feat(fix): try to fix plugin build function

--- src/build.ts
+++ src/build.ts
@@ -1,4 +1,3 @@
-import { build } from 'unbuild'
 import type { BuildConfig } from 'unbuild'
 
 interface Options {
@@ -8,27 +7,32 @@ interface Options {
   }[]
 }
 
+/**
+ * Build CkEditor plugins using unbuild
+ */
 export function buildCkEditorPlugins(args: Options): void {
-  const config: BuildConfig = {
-    entries: args.plugins.map(plugin => ({
-      builder: 'rollup',
-      input: plugin.input,
-      outDir: plugin.outDir,
-    })),
-    externals: [
-      '@ckeditor/ckeditor5-core',
-      '@ckeditor/ckeditor5-engine',
-      '@ckeditor/ckeditor5-typing',
-      '@ckeditor/ckeditor5-ui',
-      '@ckeditor/ckeditor5-utils',
-    ],
-    rollup: {
-      inlineDependencies: true,
-    },
-  };
-
   // build files using unbuild
   (async () => {
+    const { build } = await import('unbuild')
+
+    const config: BuildConfig = {
+      entries: args.plugins.map(plugin => ({
+        builder: 'rollup',
+        input: plugin.input,
+        outDir: plugin.outDir,
+      })),
+      externals: [
+        '@ckeditor/ckeditor5-core',
+        '@ckeditor/ckeditor5-engine',
+        '@ckeditor/ckeditor5-typing',
+        '@ckeditor/ckeditor5-ui',
+        '@ckeditor/ckeditor5-utils',
+      ],
+      rollup: {
+        inlineDependencies: true,
+      },
+    }
+
     await build('.', false, config)
   })()
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { buildCkEditorPlugins } from './build'
 import { injectContentToCursorPosition } from './lib/inject-content'
 import { addTextWrapButton } from './lib/text-wrap'
 
 export {
   addTextWrapButton,
+  buildCkEditorPlugins,
   injectContentToCursorPosition,
 }
